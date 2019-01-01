173e377697cc48cd81367a4dbcf765070a3ba100
dmontecino <dmontecino@ucdavis.edu>
2019-01-01 12:08:07 -0800

    Update Natural_history_script.R

--- Natural_history_script.R
+++ Natural_history_script.R
@@ -527,3 +527,5 @@ for(j in 1:num.iterations){# num of iterations is set in parameters
 
       #------------------------------------------------------#
  
+output=lapply(to.save, get)
+names(output)=to.save
