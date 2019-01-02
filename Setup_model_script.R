bd7c6785d24efddcfc15a82331b9c22af89e643b
dmontecino <dmontecino@ucdavis.edu>
2019-01-01 21:12:11 -0800

    Update Setup_model_script.R

--- Setup_model_script.R
+++ Setup_model_script.R
@@ -12,22 +12,13 @@
 ###########################
 
 # function to load packages 
-using=function(...) {
-  libs=unlist(list(...))
-  req=unlist(lapply(libs,require,character.only=TRUE))
-  need=libs[req==FALSE]
-  n=length(need)
-  if(n>0){
-    libsmsg=if(n>2) paste(paste(need[1:(n-1)],collapse=", "),",",sep="") else need[1]
-    print(libsmsg)
-    if(n>1){
-      libsmsg<-paste(libsmsg," and ", need[n],sep="")
-    }
-    libsmsg=paste("The following packages could not be found: ",libsmsg,"\n\r\n\rInstall missing packages?",collapse="")
-    if(winDialog(type = c("yesno"), libsmsg)=="YES"){       
-      install.packages(need)
-      lapply(need,require,character.only=TRUE)
-    }
+using<-function(...) {
+  libs<-unlist(list(...))
+  req<-unlist(lapply(libs,require,character.only=TRUE))
+  need<-libs[req==FALSE]
+  if(length(need)>0){ 
+    install.packages(need)
+    lapply(need,require,character.only=TRUE)
   }
 }
 
